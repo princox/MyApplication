08f9c3eb8c1fafccdbdbcd3bf29117568b07b6d3
princox <rjhoon92@naver.com>
2015-09-17 11:45:19 +0900

    third commit-

--- button/src/main/java/com/example/com/myapplication/MainActivity.java
+++ button/src/main/java/com/example/com/myapplication/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.com.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +17,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn1;
+        Button btn1, btn2, btn3, btn4;
         btn1 = (Button)findViewById(R.id.button);
+
+        btn2 = (Button)findViewById(R.id.button2);
+
+        btn3 = (Button)findViewById(R.id.button3);
+
+        btn4 = (Button)findViewById(R.id.button4);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.nate.com"));
+                startActivity(mIntent);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"));
+                startActivity(mIntent);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
+                startActivity(mIntent);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
